146fe329dc69ef44e45e525b4500b74739ab4748
Ryehean Jones <ryehean@gmail.com>
2022-02-21 19:18:39 -0500

    Added shopping cart functionality and checkout page

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -3,7 +3,7 @@ import { Container, CssBaseline } from "@mui/material";
 import  createTheme  from "@mui/material/styles/createTheme";
 import Catalog from "../../features/catalog/Catalog";
 import Header from "./Header";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Route, Switch } from "react-router-dom";
 import HomePage from "../../features/home/HomePage";
 import ProductDetails from "../../features/catalog/ProductDetails";
@@ -13,8 +13,29 @@ import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import ServerError from "../errors/ServerError";
 import NotFound from "../errors/NotFound";
+import BasketPage from "../../features/basket/BasketPage";
+import { useStoreContext } from "../context/StoreContect";
+import { getCookie } from "../util/util";
+import agent from "../api/agent";
+import LoadingComponent from "./LoadingComponents";
+import CheckoutPage from "../../features/checkout/CheckoutPage";
 
 function App() {
+  const {setBasket} = useStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .catch(error => console.log(error))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false);
   const palettetype = darkMode ? 'dark' : 'light';
 
@@ -31,6 +52,8 @@ function App() {
     setDarkMode(!darkMode);
   }
 
+  if (loading) return <LoadingComponent message='Initializing app...' />
+
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer theme='colored' position='bottom-right' hideProgressBar/>
@@ -44,6 +67,8 @@ function App() {
           <Route path='/about' component={AboutPage}/>
           <Route path='/contact' component={ContactPage}/>
           <Route path='/server-error' component={ServerError}/>
+          <Route path='/basket' component={BasketPage} />
+          <Route path='/checkout' component={CheckoutPage} />
           <Route component={NotFound}/>
         </Switch>
         
